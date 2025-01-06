9103c3534de4cbcfe436b9a5d9a6a6a7cfcad16b
nitokrisalpha <nitokrisalpha@outlook.com>
2025-01-06 21:50:34 +0800

    Jenkins HelloWorld

---
+++ src/main/java/com/nitokrisalpha/Application.java
@@ -0,0 +1,13 @@
+package com.nitokrisalpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+@SpringBootConfiguration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
